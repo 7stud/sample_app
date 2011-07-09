3f1b92c1cf24f7e0c8b6aa392f9eed22a1ac0dcc
7stud <7stud@excite.com>
2011-07-09 13:28:06 -0700

    Fixed title

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -11,7 +11,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => "Ruby Tutorial Sample App | Home")
+      response.should have_selector("title", :content => "Grant's website | Home")
     end
 
   end
@@ -24,7 +24,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "Ruby Tutorial Sample App | Contact")
+      response.should have_selector("title", :content => "Grant's website | Contact")
     end
 
   end
@@ -37,7 +37,7 @@ describe PagesController do
 
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => "Ruby Tutorial Sample App | About")
+      response.should have_selector("title", :content => "Grant's website | About")
     end
   end
 
